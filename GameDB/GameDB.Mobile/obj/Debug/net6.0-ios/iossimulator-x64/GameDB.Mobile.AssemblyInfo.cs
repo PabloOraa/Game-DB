a91c93acd60e8f084b8dc35aa0a6b24c6a0fe87b
Pablo Oraá López <pablito234@live.com>
2022-11-30 23:47:32 +0100

    Update to .NET 7 and latest NuGet packages

--- GameDB/GameDB.Mobile/obj/Debug/net6.0-ios/iossimulator-x64/GameDB.Mobile.AssemblyInfo.cs
+++ GameDB/GameDB.Mobile/obj/Debug/net6.0-ios/iossimulator-x64/GameDB.Mobile.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyProductAttribute("GameDB.Mobile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameDB.Mobile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Runtime.Versioning.TargetPlatformAttribute("iOS15.4")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("iOS16.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("iOS14.2")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.

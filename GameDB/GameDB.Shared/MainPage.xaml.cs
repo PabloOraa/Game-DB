8ac32f2826f61fe9d4df0e146ba1392ee8be8cac
Pablo Oraá López <pablito234@live.com>
2022-11-30 23:45:54 +0100

    Minor updates to search

--- GameDB/GameDB.Shared/MainPage.xaml.cs
+++ GameDB/GameDB.Shared/MainPage.xaml.cs
@@ -1,21 +1,12 @@
 ﻿using GameDB.Wrapper;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text.Json;
 using System.Text.Json.Nodes;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Web;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -39,21 +30,22 @@ namespace GameDB
         public async void getProducts()
         {
             List<Wrapper_MsStore>? msStoreResultList = new List<Wrapper_MsStore>();
-            HttpResponseMessage response = await new HttpClient().GetAsync("https://storeedgefd.dsx.mp.microsoft.com/v9.0/pages/searchResults?appVersion=22203.1401.0.0&market=US&locale=en-US&deviceFamily=windows.desktop&query=Final%20Fantasy");
+            var searchTerm = "Final Fantasy";
+            HttpResponseMessage response = await new HttpClient().GetAsync("https://storeedgefd.dsx.mp.microsoft.com/v9.0/pages/searchResults?appVersion=22203.1401.0.0&market=US&locale=en-US&deviceFamily=windows.desktop&query="+ HttpUtility.UrlEncode(searchTerm));
             if (response.IsSuccessStatusCode)
             {
-                String text = await response.Content.ReadAsStringAsync();
+                string text = await response.Content.ReadAsStringAsync();
                 if(text != null)
                 {
-                    JsonNode jsonResult = JsonArray.Parse(text);
-                    msStoreResultList.AddRange(collection: from JsonNode jsonNode in jsonResult.AsArray()
-                                               let weatherForecast = JsonSerializer.Deserialize<Wrapper_MsStore>(jsonNode)
-                                               where weatherForecast?.Payload.SearchResults != null
-                                               select weatherForecast);
+                    JsonNode? jsonResult = JsonNode.Parse(text);
+                    msStoreResultList.AddRange(collection: from JsonNode jsonNode in jsonResult!.AsArray()
+                                               let msStoreResults = JsonSerializer.Deserialize<Wrapper_MsStore>(jsonNode)
+                                               where msStoreResults?.Payload.SearchResults != null
+                                               select msStoreResults);
                 }
             }
 
-            textBlock.Text = "Encontrados " + msStoreResultList.Select(e => e.Payload.SearchResults.Count).Sum() + " resultados en la búsqueda de Final Fantasy";
+            textBlock.Text = "Encontrados " + msStoreResultList.Select(e => e.Payload.SearchResults.Count).Sum() + $" resultados en la búsqueda de {searchTerm}";
         }
     }
 }

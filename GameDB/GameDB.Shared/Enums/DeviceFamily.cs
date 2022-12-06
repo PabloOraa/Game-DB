5bce9b8ba06528ae3db1653751ed290360c5d2f3
Pablo Oraá López <pablito234@live.com>
2022-12-06 01:20:17 +0100

    Possibility to select DeviceFamily Automaticallly detects language and region to personalize the url If there's only one result, it will take the hero image and put as Background. Filter the results to show only the number of them which has the real name and not similarities.

---
+++ GameDB/GameDB.Shared/Enums/DeviceFamily.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameDB.Enums;
+enum DeviceFamily
+{
+    Desktop,
+    Mobile,
+    Xbox,
+    Hololens,
+    Hub,
+    IoT
+}
--- GameDB/GameDB.Shared/MainPage.xaml.cs
+++ GameDB/GameDB.Shared/MainPage.xaml.cs
@@ -1,12 +1,20 @@
-﻿using GameDB.Wrapper;
+﻿using GameDB.Enums;
+using GameDB.Wrapper;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Web;
+using Windows.Storage.Streams;
+using Windows.System;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -17,9 +25,15 @@ namespace GameDB
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private string Family { get; set; }
         public MainPage()
         {
             this.InitializeComponent();
+            foreach(var platform in Enum.GetValues(typeof(DeviceFamily)))
+            {
+                platforms.Items.Add(platform);
+            }
+            platforms.SelectedIndex = 0;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -29,9 +43,10 @@ namespace GameDB
 
         public async void getProducts()
         {
-            List<Wrapper_MsStore>? msStoreResultList = new List<Wrapper_MsStore>();
+            List<Wrapper_MsStore>? msStoreResultList = new ();
             var searchTerm = nameSearch.Text;
-            var url = "https://storeedgefd.dsx.mp.microsoft.com/v9.0/pages/searchResults?appVersion=22203.1401.0.0&market=US&locale=en-US&deviceFamily=windows.desktop&query=" + HttpUtility.UrlEncode(searchTerm);
+            var region = System.Threading.Thread.CurrentThread.CurrentUICulture;
+            var url = $"https://storeedgefd.dsx.mp.microsoft.com/v9.0/pages/searchResults?appVersion=22203.1401.0.0&market={region.TwoLetterISOLanguageName.ToUpper()}&locale={region.IetfLanguageTag}&deviceFamily={Family}&query={HttpUtility.UrlEncode(searchTerm)}";
             HttpResponseMessage response = await new HttpClient().GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
@@ -46,7 +61,55 @@ namespace GameDB
                 }
             }
 
-            textBlock.Text = "Encontrados " + msStoreResultList.Select(e => e.Payload.SearchResults.Count).Sum() + $" resultados en la búsqueda de {searchTerm}";
+            List<SearchResults> msStoreResultsFiltered = new();
+            foreach(var result in msStoreResultList)
+            {
+                foreach(var msStoreResult in result.Payload.SearchResults) 
+                {
+                    if(msStoreResult.Title.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) || msStoreResult.LongDescription.Contains(searchTerm,StringComparison.OrdinalIgnoreCase))
+                    {
+                        msStoreResultsFiltered.Add(msStoreResult);
+                    }
+                }
+            }
+
+            textBlock.Text = $"Encontrados {msStoreResultsFiltered.Count} resultados en la búsqueda de {searchTerm}";
+            if(msStoreResultsFiltered.Count == 1)
+            {
+                var imgUrl = new Uri(msStoreResultsFiltered[0].Previews[0].Url);
+                var responseIm = await new HttpClient().GetAsync(imgUrl);
+                byte[] img = await responseIm.Content.ReadAsByteArrayAsync();
+
+                InMemoryRandomAccessStream randomAccessStream = new();
+
+                DataWriter writer = new(randomAccessStream.GetOutputStreamAt(0));
+
+                writer.WriteBytes(img);
+
+                await writer.StoreAsync();
+
+                BitmapImage b = new();
+
+                b.SetSource(randomAccessStream);
+                backgroundImage.ImageSource = b;
+                mainWindow.Height = ActualHeight;
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combo = (ComboBox)sender; 
+
+            Family = combo.SelectedItem switch
+            {
+                DeviceFamily.Desktop => "windows.desktop",
+                DeviceFamily.Mobile => "windows.mobile",
+                DeviceFamily.Hololens => "windows.holographic",
+                DeviceFamily.Hub => "windows.team",
+                DeviceFamily.Xbox => "windows.xbox",
+                DeviceFamily.IoT => "windows.iot",
+                _ => "windows.desktop",
+            };
         }
     }
 }
